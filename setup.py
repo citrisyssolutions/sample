03e49c823fbd28558b623f7cb1ac4c443538696c
manjula-0 <rmanjula0214@gmail.com>
2022-09-19 16:45:42 +0530

    file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='BookingExample',
+    version='1.0',
+    description='Example booking application',
+    author='Greg Ward',
+    author_email='info@citrisys.com',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['booking'],
+)
